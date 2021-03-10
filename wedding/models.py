1c48b0f30b3b30b7c13fe919da4eeeddc8e0c107
achrefabdennebi <achref.abdennebi@gmail.com>
2021-03-10 20:34:41 +0100

    feat: add task model

--- wedding/models.py
+++ wedding/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 
 class User(AbstractUser):
     pass
+
+
+class Task(models.Model):
+    Title = models.CharField(max_length=64)
+    Content = models.CharField(max_length=256)
+    CreatedDate = models.DateTimeField(default=datetime.now())
+    Budget = models.FloatField()
+    Status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.Title} - {self.CreatedDate}"

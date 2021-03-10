650a8e4f647ca35ffa3b6ebabfebc315891e4ba0
achrefabdennebi <achref.abdennebi@gmail.com>
2021-03-10 19:39:09 +0100

    feat: add pieChart chartJS

--- wedding/static/wedding/index.js
+++ wedding/static/wedding/index.js
@@ -1,62 +1,97 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var calendarEl = document.getElementById('calendar');
+    const calendarEl = document.getElementById('calendar');
+    const pieChartEl = document.getElementById('pieChart');
 
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        initialDate: '2021-02-07',
-        headerToolbar: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay'
-        },
-        events: [{
-                title: 'All Day Event',
-                start: '2021-02-01'
+    if (calendarEl) {
+        const calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            initialDate: '2021-02-07',
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
             },
-            {
-                title: 'Long Event',
-                start: '2021-02-07',
-                end: '2021-02-10'
-            },
-            {
-                groupId: '999',
-                title: 'Repeating Event',
-                start: '2021-02-09T16:00:00'
-            },
-            {
-                groupId: '999',
-                title: 'Repeating Event',
-                start: '2021-02-16T16:00:00'
-            },
-            {
-                title: 'Conference',
-                start: '2021-02-11',
-                end: '2021-02-13'
-            },
-            {
-                title: 'Meeting',
-                start: '2021-02-12T10:30:00',
-                end: '2021-02-12T12:30:00'
-            },
-            {
-                title: 'Lunch',
-                start: '2021-02-12T12:00:00'
-            },
-            {
-                title: 'Meeting',
-                start: '2021-02-12T14:30:00'
-            },
-            {
-                title: 'Birthday Party',
-                start: '2021-02-13T07:00:00'
-            },
-            {
-                title: 'Click for Google',
-                url: 'http://google.com/',
-                start: '2021-02-28'
-            }
-        ]
-    });
+            events: [{
+                    title: 'All Day Event',
+                    start: '2021-02-01'
+                },
+                {
+                    title: 'Long Event',
+                    start: '2021-02-07',
+                    end: '2021-02-10'
+                },
+                {
+                    groupId: '999',
+                    title: 'Repeating Event',
+                    start: '2021-02-09T16:00:00'
+                },
+                {
+                    groupId: '999',
+                    title: 'Repeating Event',
+                    start: '2021-02-16T16:00:00'
+                },
+                {
+                    title: 'Conference',
+                    start: '2021-02-11',
+                    end: '2021-02-13'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2021-02-12T10:30:00',
+                    end: '2021-02-12T12:30:00'
+                },
+                {
+                    title: 'Lunch',
+                    start: '2021-02-12T12:00:00'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2021-02-12T14:30:00'
+                },
+                {
+                    title: 'Birthday Party',
+                    start: '2021-02-13T07:00:00'
+                },
+                {
+                    title: 'Click for Google',
+                    url: 'http://google.com/',
+                    start: '2021-02-28'
+                }
+            ]
+        });
 
-    calendar.render();
+        calendar.render();
+    }
+
+    if (pieChartEl) {
+        const dataPieChart = {
+            datasets: [{
+                data: [10, 20, 30],
+                backgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ],
+                hoverBackgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ]
+            }],
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'Red',
+                'Yellow',
+                'Blue'
+            ]
+        }
+
+        const myPieChart = new Chart(pieChartEl, {
+            type: 'pie',
+            data: dataPieChart,
+            options: {}
+        });
+
+    }
+
 });

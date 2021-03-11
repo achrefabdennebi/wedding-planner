c63f7ae19203b49fe0069f8cdec873024aa83d23
achrefabdennebi <achref.abdennebi@gmail.com>
2021-03-11 19:24:55 +0100

    feat: edit input type in content

--- wedding/admin.py
+++ wedding/admin.py
@@ -1,6 +1,18 @@
+from django import forms
 from django.contrib import admin
 from .models import User, Task
+from django.forms import Textarea
+
 # Register your models here.
 
+class TaskForm(forms.ModelForm):
+    Content = forms.CharField( widget=forms.Textarea(attrs={'rows': 5, 'cols': 50}))
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+class TaskAdmin(admin.ModelAdmin):
+    form = TaskForm
+
 admin.site.register(User)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)

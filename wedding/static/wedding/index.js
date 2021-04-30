30a2c6499bfe9f588d450c354f7b17a2d620da53
achrefabdennebi <achref.abdennebi@gmail.com>
2021-04-30 20:23:47 +0200

    feat: intlTelInput

--- wedding/static/wedding/index.js
+++ wedding/static/wedding/index.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const formTask = document.getElementById('formTask');
     const buttons  = document.querySelectorAll('button');
+    const input = document.querySelector("#inputPhone");
+
+    if (input) {
+        window.intlTelInput(input, {
+            utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/utils.js",
+        });
+    }
 
     if (formTask) {
         formTask.addEventListener("submit", function (e) {
